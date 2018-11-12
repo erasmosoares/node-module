4c8c7d1f8984bcc795b15e08f9f852311bd707ff
Erasmo Soares <esaj@cesar.org.br>
2018-11-12 16:38:51 -0300

    Path module

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+// Give path information
+const path = require('path'); 
+var pathObj = path.parse(__filename);
+console.log(pathObj);
+
+// Creating a module
 const log = require('./logger');
-
 console.log(log);
-//console.log(module);
-
 log('hey');
