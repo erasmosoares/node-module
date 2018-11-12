b3e42ccf19739cc038f693ba11550cbdb87b960f
Erasmo Soares <esaj@cesar.org.br>
2018-11-12 16:57:04 -0300

    file system

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-// Give path information
-const path = require('path'); 
-var pathObj = path.parse(__filename);
-console.log(pathObj);
-
 // Creating a module
 const log = require('./logger');
 console.log(log);
 log('hey');
+
+// Give path information
+const path = require('path'); 
+var pathObj = path.parse(__filename);
+log(pathObj);
+
+// OS Info
+const os = require('os');
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+
+//Template string ES6
+log(`Total Memory: ${totalMemory}`);
+log(`Free Memory: ${freeMemory}`);
+
+// File System
+const fs = require('fs');
+const files = fs.readdirSync('./');
+fs.readdir('./',function(err,files){
+    if(err) log(`Error:${err}`);
+    else log(`Result:${files}`);
+});
+
+log(files);
