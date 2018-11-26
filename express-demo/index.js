e06956429562a27063bc085ca8dc51018c8481e4
Erasmo Soares <esaj@cesar.org.br>
2018-11-26 10:51:06 -0300

    Http get requests

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+    { id:1, name:'course 1'},
+    { id:2, name:'course 2'},
+    { id:3, name:'course 3'},
+];
 /*
 app.get();
 app.post();
@@ -13,7 +18,20 @@ app.get('/',(req,res)=>{
 });
 
 app.get('/api/courses',(req,res)=>{
-    res.send([1,2,3]);
+    res.send(courses);
+});
+
+//http://localhost:3000/api/courses/1
+app.get('/api/courses/:id',(req,res) =>{
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    
+    if(!course) res.status(404).send('The course with the given id was not found');
+    res.send(course);
+});
+
+//http://localhost:3000/api/posts/2018/01
+app.get('/api/posts/:year/:month',(req,res) =>{
+    res.send(req.params); //req.query
 });
 
 // PORT
