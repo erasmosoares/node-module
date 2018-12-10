ae9010e91862cd60de8762ada2886eed958f997e
Erasmo Soares <esaj@cesar.org.br>
2018-12-10 16:31:30 -0300

    Async operation

---
+++ async-demo/parallel-promise.js
@@ -0,0 +1,13 @@
+const p1 = new Promise((resolve)=>{
+    setTimeout(()=>{
+        console.log('Async operation 1...');
+        resolve(1);
+    },2000);
+});
+
+const p2 = new Promise((resolve)=>{
+    setTimeout(()=>{
+        console.log('Async operation 2...');
+        resolve(2);
+    },2000);
+});
