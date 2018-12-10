3de1293253acd9f7d6dc8c56779c6fb4e82e660b
Erasmo Soares <esaj@cesar.org.br>
2018-12-10 12:51:09 -0300

    Refactoring callbacks to promises

--- async-demo/index.js
+++ async-demo/index.js
@@ -12,18 +12,22 @@ function displayCommits(commits){
 }
 console.log('After');
 
-function getUser(id, callback){
-    setTimeout(()=>{
-        console.log('Reading a user from a database...');
-        callback({ id:id, gitHubUsername:'erasmo'});
-    },2000);
+function getUser(id){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log('Reading a user from a database...');
+            resolve({ id:id, gitHubUsername:'erasmo'});
+        },2000);
+    });
 }
 
-function getRepositories(username,callback){
-    setTimeout(()=>{
-        console.log('Calling GithubApi...');
-        callback(['repo1','repo2','repo3']);
-    },2000);
+function getRepositories(username){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log('Calling GithubApi...');
+            resolve(['repo1','repo2','repo3']);
+        },2000);
+    });
 }
 /*console.log('Before');
 getUser(1, (user) =>{
