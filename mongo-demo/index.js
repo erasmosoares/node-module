c9944d889d5b9db5c61a2baa8279204a5b3e529c
Erasmo Soares <esaj@cesar.org.br>
2018-12-11 13:53:07 -0300

    Comparison operators

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -11,3 +11,43 @@ const courseSchema = new mongoose.Schema({
     date: {type:Date,default:Date.now},
     isPublished:Boolean
 });
+
+const Course = mongoose.model('Course',courseSchema);
+
+async function createCourse(){
+
+    const course = new Course({
+        name:'Angular Course',
+        author:'Esaj',
+        tags:['angular','frontend'],
+        isPublished:true
+    });
+    
+    const result = await course.save();
+    console.log(result);
+}
+
+//eq (equal)
+//ne (not equal)
+//gt (greater than)
+//gte (greater than or equal to)
+//lt (less than)
+//lte (less than or equal to)
+//in
+//nin (not in)
+
+async function getCourses(){
+//    const courses = await Course.find();
+    const courses = await Course
+    //.find({author:'Esaj', isPublished:true})
+    //.find({price: 10})
+    //.find({price: {$gt:10, $lte: 20}})
+    .find({price: {$in:[10,15,30]}})
+    .limit(10)
+    .sort({name:1}) //1=ascending -1=descending
+    .select({name:1,tags:1});
+    
+    console.log(courses);
+}
+
+getCourses();
